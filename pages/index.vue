e15b069d72841d5f8ba363e90626e5f54c9fe627
Aaron Wright <aaron.c.wright@icloud.com>
2023-01-13 18:48:07 +0000

    Add index page

---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <main class="flex items-center justify-center bg-gradient-to-br from-zinc-800 to-zinc-900">
+    <img src="~/assets/CashierFu.svg" alt="CashierFu" id="logo" class="w-40"/>
+  </main>
+</template>
