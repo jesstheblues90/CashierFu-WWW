4f3eaa7d24b8eb79cde26ccc497fb9bcd1081ee7
Aaron Wright <aaron.c.wright@icloud.com>
2024-01-09 10:39:16 -0600

    Set up

---
+++ content/docs/index.md
@@ -0,0 +1,6 @@
+CashierFu
+=========
+
+Need help? Contact us at [support@cashierfu.com](mailto:support@cashierfu.com)
+
+[&copy; 2023 Infinite Token LLC](https://infinitetoken.com) | [Privacy Policy](/privacy)
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <Header />
-  <Main class="items-center grow">
-    <Logo class="w-32 mt-8 mb-4" />
+  <Main class="items-center justify-center grow">
+    <Logo class="w-32" />
   </Main>
   <Footer />
 </template>
